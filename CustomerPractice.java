b36ed330b2fe62431e95a6cf1874eaefce30ea57
elam3 <elam3@mail.ccsf.edu>
2018-04-22 20:47:48 -0700

    initial commit

---
+++ CustomerPractice.java
@@ -0,0 +1,51 @@
+import java.util.List;
+
+public class CustomerPractice {
+
+	private String firstName, lastName;
+	private int age;
+	private Gender gender;
+	private Address address;
+	private List<Order> orders;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public List<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	
+	
+}
